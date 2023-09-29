a263f78d9772b7e4d0ea6e51361fe1556ff77cad
bintangrpl <87853827+Vinobin@users.noreply.github.com>
2023-09-29 09:38:56 +0700

    V 1.3
    JAVASCRIPT PEMROGRAMAN WEB : DOM (SELECTOR)

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
 function tampil() {
-   document.getElementById("saya").innerHTML = "javascript";
+   document.querySelector("h1").innerText = "belajar javascript";
+   document.querySelector(".container").style.backgroundColor = "green";
+   document.querySelector("#lorem").style.fontSize = "100px"
 }
